1e3c4974050c8408d2ba21ccf9f27185b28abd6e
Hugo van Duijn <hugo.vanduijn@naturalis.nl>
2019-02-28 08:53:31 +0000

    added script

---
+++ pgps.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Script to see what is taking so long in the postgres database
+cd /opt/ppdb
+docker-compose exec -T postgres psql postgres postgres -c "SELECT pid, now() - pg_stat_activity.query_start AS duration, query, state FROM pg_stat_activity WHERE (now() - pg_stat_activity.query_start) > interval '10 seconds';" 
+cd - 
